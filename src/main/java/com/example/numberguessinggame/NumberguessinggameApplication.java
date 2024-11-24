f2920f9ef54d18f3294834f4030aab646299dace
SHIN SUJUNG <shinssjj79@gmail.com>
2024-11-24 11:47:03 +0900

    Initial commit

---
+++ src/main/java/com/example/numberguessinggame/NumberguessinggameApplication.java
@@ -0,0 +1,13 @@
+package com.example.numberguessinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberguessinggameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberguessinggameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/numberguessinggame/NumberguessinggameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numberguessinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberguessinggameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
